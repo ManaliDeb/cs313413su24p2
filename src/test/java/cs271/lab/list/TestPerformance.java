83eb3d48f4cd312d1a1640fb43bcda876b5fbe77
Manali Deb <Debook4@gmail.com>
2024-06-20 01:58:21 -0500

    Created JUnit test for performance testing

--- src/test/java/cs271/lab/list/TestPerformance.java
+++ src/test/java/cs271/lab/list/TestPerformance.java
@@ -14,11 +14,11 @@ public class TestPerformance {
   // running time is in the tens of seconds)
   // TODO (optional) refactor to DRY
   // which of the two lists performs better as the size increases?
-  private final int SIZE = 10;
+  private final int[] SIZES = {10, 100, 1000, 10000, 100000};
 
   // TODO choose this value in such a way that you can observe an actual effect
   // for increasing problem sizes
-  private final int REPS = 1000000;
+  private final int REPS = 100000;
 
   private List<Integer> arrayList;
 
@@ -26,12 +26,8 @@ public class TestPerformance {
 
   @Before
   public void setUp() throws Exception {
-    arrayList = new ArrayList<Integer>(SIZE);
+    arrayList = new ArrayList<Integer>();
     linkedList = new LinkedList<Integer>();
-    for (var i = 0; i < SIZE; i++) {
-      arrayList.add(i);
-      linkedList.add(i);
-    }
   }
 
   @After
@@ -40,35 +36,70 @@ public class TestPerformance {
     linkedList = null;
   }
 
+  private void fillList(int size){
+    arrayList.clear();
+    linkedList.clear();
+    for(var i = 0; i < size; i++){
+      arrayList.add(i);
+      linkedList.add(i);
+    }
+  }
+
   @Test
-  public void testLinkedListAddRemove() {
+  public void runTests(){
+    for (int size : SIZES) {
+      System.out.println("test with size " + size);
+
+      fillList(size);
+
+      long startTime = System.currentTimeMillis();
+      testLinkedListAddRemove();
+      long endTime = System.currentTimeMillis();
+      System.out.println("LinkedList add/remove: " + (endTime - startTime) + " ms");
+
+      startTime = System.currentTimeMillis();
+      testArrayListAddRemove();
+      endTime = System.currentTimeMillis();
+      System.out.println("ArrayList add/remove: " + (endTime - startTime) + " ms");
+
+      startTime = System.currentTimeMillis();
+      testLinkedListAccess();
+      endTime = System.currentTimeMillis();
+      System.out.println("LinkedList access: " + (endTime - startTime) + " ms");
+
+      startTime = System.currentTimeMillis();
+      testArrayListAccess();
+      endTime = System.currentTimeMillis();
+      System.out.println("ArrayList access: " + (endTime - startTime) + " ms");
+      System.out.println();
+    }
+  }
+
+  private void testLinkedListAddRemove() {
     for (var r = 0; r < REPS; r++) {
       linkedList.add(0, 77);
       linkedList.remove(0);
     }
   }
 
-  @Test
-  public void testArrayListAddRemove() {
+  private void testArrayListAddRemove() {
     for (var r = 0; r < REPS; r++) {
       arrayList.add(0, 77);
       arrayList.remove(0);
     }
   }
 
-  @Test
-  public void testLinkedListAccess() {
+  private void testLinkedListAccess() {
     var sum = 0L;
     for (var r = 0; r < REPS; r++) {
-      sum += linkedList.get(r % SIZE);
+      sum += linkedList.get(r % linkedList.size());
     }
   }
 
-  @Test
-  public void testArrayListAccess() {
+  private void testArrayListAccess() {
     var sum = 0L;
     for (var r = 0; r < REPS; r++) {
-      sum += arrayList.get(r % SIZE);
+      sum += arrayList.get(r % arrayList.size());
     }
   }
 }
